1ddd033e360aa88af4b646a074b00e37ccb3a591
Devin Fan <dbfan@andrew.cmu.edu>
2018-08-20 16:57:14 -0700

    macOS: Created NSTableView in ViewController

--- MangaReader/ViewController.swift
+++ MangaReader/ViewController.swift
@@ -10,9 +10,12 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var tableView: NSTableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.dataSource = self
+        tableView.delegate = self
         // Do any additional setup after loading the view.
     }
 
@@ -22,5 +25,27 @@ class ViewController: NSViewController {
         }
     }
 
+}
+
+extension ViewController: NSTableViewDataSource {
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return 10
+    }
+}
+
+extension ViewController: NSTableViewDelegate {
+    fileprivate enum CellIdentifiers {
+        static let MangaCell = "MangaCellID"
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        let cellIdentifier = NSUserInterfaceItemIdentifier(rawValue: CellIdentifiers.MangaCell)
+        if let cell = tableView.makeView(withIdentifier: cellIdentifier, owner: nil) as? NSTableCellView {
+            cell.textField?.stringValue = "Hello"
+            cell.imageView?.image = nil ?? nil
+            return cell
+        }
+        return nil
+    }
 }
 
