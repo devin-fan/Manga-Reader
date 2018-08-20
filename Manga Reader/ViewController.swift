64c3a443db8e18108b4b1536f585dfa5ef700bed
Devin Fan <dfan888@gmail.com>
2018-08-20 15:46:48 -0700

    Initial Commit

---
+++ Manga Reader/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Manga Reader
+//
+//  Created by Devin Fan on 8/20/18.
+//  Copyright © 2018 Devin Fan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
